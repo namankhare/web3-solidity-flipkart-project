b2c35124972b350a748fc6b2456cc727a24a8b7c
Sucheta <pal.sucheta2@gmail.com>
2023-08-19 14:17:31 +0530

    Launch products

--- client/src/core/seller/Product.jsx
+++ client/src/core/seller/Product.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { AddProduct } from "../../module/products/AddProduct";
+import { DeleteProduct } from "../../module/products/DeleteProduct";
+import { ViewProduct } from "../../module/products/ViewProduct";
 
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <>
+      <div>Product</div>
+      <AddProduct/>
+      <DeleteProduct/>
+      <ViewProduct/>
+    </>
+  );
 };
 
 export default Product;
