4de582980425f0b9b4ed3834fc336b1984fe4975
Sucheta <pal.sucheta2@gmail.com>
2023-08-19 12:32:03 +0530

    Login and signup page

--- client/src/core/auth/Signup.jsx
+++ client/src/core/auth/Signup.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import "../../assets/css/auth.css";
+import signUp from "../../assets/img/sammy-25.png";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const navigate = useNavigate();
+
+  return (<div class="form-container sign-up-form">
+  <div class="imgBox sign-up-imgBox">
+      <div class="sliding-link">
+          <p>Already have an account?</p>
+          <span class="sign-in-btn" onClick={()=>{navigate("/login")}}>Sign in</span>
+      </div>
+      <img src={signUp} alt=""/>
+  </div>
+  <div class="form-box sign-up-box">
+      <h2>Sign up</h2>
+
+      <form action="/signup" method="post">
+          <div class="field">
+              <i class="fa-solid fa-at"></i>
+              <input type="email" name="email" placeholder="Enter email ID" required/>
+          </div>
+          <div class="field">
+              <i class="fa-solid fa-user"></i>
+              <input type="text" name="name" placeholder="Enter your name" required/>
+          </div>
+          <div class="field">
+              <i class="fa-solid fa-user"></i>
+              <input type="text" name="username" placeholder="Enter username" required/>
+          </div>
+          <div class="field">
+              <i class="fa-solid fa-key"></i>
+              <input type="password" name="password" placeholder="Enter password" required/>
+
+          </div>
+          <div class="field">
+              <i class="fa-solid fa-key"></i>
+              <input type="password" name="confirm-password" placeholder="Confirm password" required/>
+
+          </div>
+
+          <input type="submit" class="submit-btn" value="Sign up"/>
+      </form>
+
+
+
+  </div>
+
+  </div>);
 };
 
 export default Signup;
