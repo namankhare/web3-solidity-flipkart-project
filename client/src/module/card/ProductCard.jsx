fa67b5452c30f0ed639012c228621ffc395eabd8
Naman Khare <hi@namankhare.com>
2023-08-20 03:12:28 +0530

    fix image

--- client/src/module/card/ProductCard.jsx
+++ client/src/module/card/ProductCard.jsx
@@ -3,9 +3,8 @@ import { useContext, useState } from 'react'
 import { GlobalContext } from '../../context/GlobalContext'
 import apiClient from '../../helper/apiClient'
 import { PopUp } from '../products/PopUp'
-import laddoo from '../../assets/img/laddoo.jpg'
 
-const ProductCard = ({item,setProducts,products}) => {
+const ProductCard = ({ item, setProducts, products }) => {
   const { authUser } = useContext(GlobalContext);
 
   const handleDelete = async () => {
@@ -29,24 +28,24 @@ const ProductCard = ({item,setProducts,products}) => {
   return (
     <>
       <div className="card col-12 col-md-6 col-lg-3 ">
-            <img src={laddoo} className="card-img-top mt-3" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">{authUser.name}</h5>
-              <p className="card-text">{item.name}</p>
-              <p className="card-text">{item.description}</p>
-              <p className="card-text"><s><b>MRP: </b>Rs.{item.MRP}</s></p>
-              <p className="card-text"><b>Current Price: </b>{currentPrice}</p>
-              <p className="card-text"><b>FLT can be used to further lower the price: </b>{currentPrice}-{item.points}% = {discountFinal}</p>
-              <p className="card-text"><b>In Stock: </b>{item.SKU}</p>
-              <div className="d-flex justify-content-between">
-              <button className='btn btn-primary' onClick={handleDelete}>Delete</button>
-              <button className='btn btn-primary' onClick={handleUpdate}>Update</button>
-              </div>
-            </div>
+        <img src={`${import.meta.env.VITE_BACKEND_IMAGE_URL}/uploads/products/${item.productImage}`} className="card-img-top mt-3" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{authUser.name}</h5>
+          <p className="card-text">{item.name}</p>
+          <p className="card-text">{item.description}</p>
+          <p className="card-text"><s><b>MRP: </b>Rs.{item.MRP}</s></p>
+          <p className="card-text"><b>Current Price: </b>{currentPrice}</p>
+          <p className="card-text"><b>FLT can be used to further lower the price: </b>{currentPrice}-{item.points}% = {discountFinal}</p>
+          <p className="card-text"><b>In Stock: </b>{item.SKU}</p>
+          <div className="d-flex justify-content-between">
+            <button className='btn btn-primary' onClick={handleDelete}>Delete</button>
+            <button className='btn btn-primary' onClick={handleUpdate}>Update</button>
+          </div>
         </div>
-        {
-          updatePopup && (<PopUp isVisiblePop = {updatePopup} setVisiblePop = {setUpdatePopup} products={products} setProducts={setProducts} type={item._id}/>)
-        }
+      </div>
+      {
+        updatePopup && (<PopUp isVisiblePop={updatePopup} setVisiblePop={setUpdatePopup} products={products} setProducts={setProducts} type={item._id} />)
+      }
     </>
   )
 }
