c8a288b473340ed1e07cc0f25a0f4645bb9cfc08
Naman Khare <hi@namankhare.com>
2023-08-19 12:37:04 +0530

    added env in background

---
+++ client/src/helper/apiClient.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("token")}`
+const apiClient = axios.create({
+    baseURL: import.meta.env.BACKEND_URL || "http://localhost:5000/api",
+    withCredentials: true,
+    headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+    },
+});
+
+//inerceptor
+apiClient.interceptors.response.use(
+    (config) => {
+        return config;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+        if (
+            error.response.status === 401 &&
+            originalRequest &&
+            !originalRequest._isRetry
+        ) {
+            originalRequest._isRetry = true;
+            try {
+                let getRefreshToken = localStorage.getItem("refreshtoken");
+                const { data } = await axios.post(
+                    `${import.meta.env.BACKEND_URL}/auth/renewAccessToken`, { refreshToken: getRefreshToken },
+                    {
+                        withCredentials: true,
+                        headers: {
+                            "Content-type": "application/json",
+                            Accept: "application/json",
+                        },
+                    }
+                );
+                // console.log("df", data)
+                localStorage.setItem("refreshtoken", data.refreshToken);
+                localStorage.setItem('token', data.token);
+                error.response.config.headers['Authorization'] = `Bearer ${data.token}`
+                axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`
+                apiClient.defaults.headers.common['Authorization'] = `Bearer ${data.token}`
+                return apiClient.request(originalRequest);
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        throw error;
+    }
+);
+
+export default apiClient;
---
+++ client/src/helper/helpFunc.js
@@ -0,0 +1,36 @@
+function delete_cookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function fromNow(date, nowDate = Date.now(), rft = new Intl.RelativeTimeFormat(undefined, { numeric: "auto" })) {
+    const SECOND = 1000;
+    const MINUTE = 60 * SECOND;
+    const HOUR = 60 * MINUTE;
+    const DAY = 24 * HOUR;
+    const WEEK = 7 * DAY;
+    const MONTH = 30 * DAY;
+    const YEAR = 365 * DAY;
+    const intervals = [
+        { ge: YEAR, divisor: YEAR, unit: 'year' },
+        { ge: MONTH, divisor: MONTH, unit: 'month' },
+        { ge: WEEK, divisor: WEEK, unit: 'week' },
+        { ge: DAY, divisor: DAY, unit: 'day' },
+        { ge: HOUR, divisor: HOUR, unit: 'hour' },
+        { ge: MINUTE, divisor: MINUTE, unit: 'minute' },
+        { ge: 30 * SECOND, divisor: SECOND, unit: 'seconds' },
+        { ge: 0, divisor: 1, text: 'just now' },
+    ];
+    const now = typeof nowDate === 'object' ? nowDate.getTime() : new Date(nowDate).getTime();
+    const diff = now - (typeof date === 'object' ? date : new Date(date)).getTime();
+    const diffAbs = Math.abs(diff);
+    for (const interval of intervals) {
+        if (diffAbs >= interval.ge) {
+            const x = Math.round(Math.abs(diff) / interval.divisor);
+            const isFuture = diff < 0;
+            return interval.unit ? rft.format(isFuture ? x : -x, interval.unit) : interval.text;
+        }
+    }
+}
+
+
+module.exports = { delete_cookie, fromNow }
