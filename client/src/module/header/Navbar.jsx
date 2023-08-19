45960353051763b1ca243980ca2be5a428233580
Naman Khare <hi@namankhare.com>
2023-08-19 21:31:21 +0530

    removed web3 import from main

--- client/src/module/header/Navbar.jsx
+++ client/src/module/header/Navbar.jsx
@@ -62,6 +62,7 @@ const Navbar = () => {
       }
     };
     checkConnection();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoggedIn]);
   const connectToMetamask = async () => {
     try {
